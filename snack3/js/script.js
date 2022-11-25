32544dc8ae06fd5200b074c3ca60b7e88bc10a0b
AntoninoIrrera <irrera.antonio2003@gmail.com>
2022-11-25 17:10:46 +0100

    snack3 terza parte

--- snack3/js/script.js
+++ snack3/js/script.js
@@ -6,6 +6,7 @@
 
     Calcola la somma e la media dei primi 10 numeri di un array.
 
+    Calcola la somma, la media e il valore massimo dei primi 10 numeri di un array.
 */
 
 
@@ -13,11 +14,17 @@ let numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 let somma = 0;
 
+let confronto = 0;
+let sostituzione = 0; 
+
 for(let i = 0; i < numeri.length; i++){
 
 
     somma += numeri[i];
 
+    if(confronto < numeri[i]){
+        confronto = numeri[i];
+    }
 
 
 }
@@ -26,3 +33,4 @@ const media = somma / numeri.length;
 
 console.log(somma);
 console.log(media);
+console.log(confronto);
