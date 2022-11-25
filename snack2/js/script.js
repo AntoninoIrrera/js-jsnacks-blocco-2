8937e4976e5770d880aadb31d8f6359248e2d3a3
AntoninoIrrera <irrera.antonio2003@gmail.com>
2022-11-25 16:01:31 +0100

    snack2

---
+++ snack2/js/script.js
@@ -0,0 +1,17 @@
+
+
+/*
+
+    Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+*/
+
+
+const numeroUtente = parseInt(prompt("inserisci un numero"));
+
+
+if(numeroUtente % 2 == 0){
+    console.log(numeroUtente);
+}else{
+    console.log(numeroUtente + 1);
+}
