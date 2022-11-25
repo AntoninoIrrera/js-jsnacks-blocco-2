c501c583edbb4427a8e687e57414d211bf770eeb
AntoninoIrrera <irrera.antonio2003@gmail.com>
2022-11-25 16:31:53 +0100

    snack3 (prima parte)

---
+++ snack3/js/script.js
@@ -0,0 +1,23 @@
+
+
+/*
+
+    Calcola la somma dei primi 10 numeri di un array.
+
+
+*/
+
+
+let numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let somma = 0;
+
+for(let i = 0; i < numeri.length; i++){
+
+
+    somma += numeri[i];
+
+
+}
+
+console.log(somma);
